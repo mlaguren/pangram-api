982e92739262a86e6a6c4e7adbab06afd53c27c8
Melvin Laguren <melvin@laguren.net>
2018-03-05 20:46:24 -0800

    Add more details for responses

--- app.rb
+++ app.rb
@@ -1,13 +1,24 @@
 require 'sinatra'
-require 'pangram'
 require 'json'
 load 'lib/pangram.rb'
 
 post "/pangram" do
-  if pangram('test')  == true
-      status 200
+  request.body.rewind
+  text = JSON.parse request.body.read
+  if text['string'].length < 26
+    status 404
+    content_type :json
+    { :message => 'Not Enough Characters In String' }.to_json
   else
-      status 404
+    if pangram(text['string'])  == true
+      status 200
+      content_type :json
+      { :pangram => 'true' }.to_json
+    else
+      status 200
+      content_type :json
+      { :pangram => 'false' }.to_json
+    end
   end
-      
+
 end
